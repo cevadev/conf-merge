b7ce222fc1df75d12a6b345a481b35daa3888cc0
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-12-01 19:15:27 -0300

    [add] Helmet tags

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import AppContext from "../context/AppContext.js";
 
@@ -30,37 +31,42 @@ function Checkout() {
   }
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {/**validamos que haya productos en el cart para mostrar los items */}
-        {cart.length > 0 ? (
-          <h3>Lista de pedidos: </h3>
-        ) : (
-          <h3>Sin pedidos que mostrar</h3>
-        )}
-        {cart.map((item, index) => (
-          <div className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>$. {item.price}</span>
+    <Fragment>
+      <Helmet>
+        <title>Lista de pedido - Store Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {/**validamos que haya productos en el cart para mostrar los items */}
+          {cart.length > 0 ? (
+            <h3>Lista de pedidos: </h3>
+          ) : (
+            <h3>Sin pedidos que mostrar</h3>
+          )}
+          {cart.map((item, index) => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>$. {item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item, index)}>
+                {/**colocamos icono de font awesome de eliminar item */}
+                <i className="fas fa-trash-alt" />
+              </button>
             </div>
-            <button type="button" onClick={handleRemove(item, index)}>
-              {/**colocamos icono de font awesome de eliminar item */}
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
-        ))}
-      </div>
-      {/**validamos que hay por lo menos un producto en el cart para mostrar total y continuar pedido */}
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio total: $. ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continua pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {/**validamos que hay por lo menos un producto en el cart para mostrar total y continuar pedido */}
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio total: $. ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continua pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 }
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
 
 import Products from "../components/Products.js";
 //importamos nuestro archivo con datos de prueba
 import initialState from "../initialState.js";
 
 function Home() {
-  return <Products products={initialState.products} />;
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Conf Merch - Products</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@harpazos" />
+        <meta name="twitter:creator" content="@harpazos" />
+        <meta name="twitter:title" content="Conf Store" />
+        <meta name="twitter:description" content="Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Conf Store" />
+        <meta property="og:description" content="Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="https://store-merch.web.app/" />
+        <meta property="og:site_name" content="Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Helmet>
+      <Products products={initialState.products} />
+    </Fragment>
+  );
 }
 
 export default Home;
